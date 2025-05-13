bc4d263dfb09d24bf00d6ee12352fdd505719fcd
Antony Vasiliev <tony.vasiliev8@gmail.com>
2025-05-12 20:11:13 -0400

    Added writing to file function to logger

--- utils/logger/logger.py
+++ utils/logger/logger.py
@@ -1,15 +1,21 @@
+import os
 from datetime import datetime
 
 class Logger:
     def __init__(self, logfile):
         self._logfile = logfile
+        self._created = False
 
     def _log(self, message):
         message = f'{datetime.now().isoformat()} - {message}'
-
         print(message)
-        # with open(self._logfile, 'a') as f:
-        #     f.write(message + '\n')
+
+        if not self._created:
+            os.makedirs(os.path.dirname(self._logfile), exist_ok=True)
+            self._created = True
+
+        with open(self._logfile, 'a') as f:
+            f.write(message + '\n')
 
     def log_info(self, message):
         self._log(f'INFO: {message}')
