e2bdf85c811a407776ea0ffab2c970294aaea3c2
Antony Vasiliev <tony.vasiliev8@gmail.com>
2025-05-12 20:11:47 -0400

    Added thread sleep for limit checkers, added default values in case optional config values are not set

--- nvrd.py
+++ nvrd.py
@@ -29,10 +29,12 @@ def setup_recorder(storage_dirpath:str, name:str, config:dict):
         SEGMENT_DURATION_KEY = 'segment-duration-sec'
         RECORD_AUDIO_KEY = 'record-audio'
 
+        RECORD_AUDIO_DEFAULT = True
+
         monitor_dirpath = os.path.join(storage_dirpath, name)
         source = config[SOURCE_KEY]
         segment_duration_sec = int(config[SEGMENT_DURATION_KEY])
-        record_audio = config[RECORD_AUDIO_KEY]
+        record_audio = config[RECORD_AUDIO_KEY] if RECORD_AUDIO_KEY in config else RECORD_AUDIO_DEFAULT
 
         if segment_duration_sec <= 0:
             raise Exception(f'Segment duration cannot be negative or zero!')
@@ -77,13 +79,21 @@ def setup_limit_checkers(recorders:dict, config:dict):
         recorder_config = config[MONITORS_KEY][name]
 
         # Get recorder age and disk limits
-        max_age_sec = float(recorder_config[RECORDER_MAX_AGE_KEY]) * 3600 if recorder_config[RECORDER_MAX_AGE_KEY] is not None else None
-        if max_age_sec is not None and max_age_sec < 0:
-            raise Exception(f'Max age cannot be negative!')
+        if RECORDER_MAX_AGE_KEY not in recorder_config or recorder_config[RECORDER_MAX_AGE_KEY] is None:
+            max_age_sec = None
+        else:
+            max_age_sec = float(recorder_config[RECORDER_MAX_AGE_KEY]) * 3600
+            
+            if max_age_sec < 0:
+                raise Exception(f'Max age cannot be negative!')
 
-        max_disk_bytes = float(recorder_config[RECORDER_MAX_DISK_KEY]) * 1e9 if recorder_config[RECORDER_MAX_DISK_KEY] is not None else None
-        if max_disk_bytes is not None and max_disk_bytes < 0:
-            raise Exception(f'Max disk cannot be negative!')
+        if RECORDER_MAX_DISK_KEY not in recorder_config or recorder_config[RECORDER_MAX_DISK_KEY] is None:
+            max_disk_bytes = None
+        else:
+            max_disk_bytes = float(recorder_config[RECORDER_MAX_DISK_KEY]) * 1e9
+
+            if max_disk_bytes < 0:
+                raise Exception(f'Max disk cannot be negative!')
 
         # Create recorder limit checker
         limit_logger = Logger(os.path.join(LOG_DIRPATH, name, 'limit.log'))
@@ -96,9 +106,9 @@ def setup_limit_checkers(recorders:dict, config:dict):
 
     return limit_checkers
 
-def start_limit_checkers(limit_checkers:list[LimitManager], run_flag:threading.Event, idle_flag:threading.Event):
-    while run_flag.is_set():
-        idle_flag.clear()
+def start_limit_checkers(limit_checkers:list[LimitManager], stop_event:threading.Event, idle_event:threading.Event):
+    while not stop_event.is_set():
+        idle_event.clear()
 
         for limit_checker in limit_checkers:
             try:
@@ -106,8 +116,9 @@ def start_limit_checkers(limit_checkers:list[LimitManager], run_flag:threading.E
             except Exception as e:
                 main_logger.log_warning(f'Failed to check limits: {e}')
 
-        time.sleep(5)
-        idle_flag.set()
+        # Wait for a minute or until event is set
+        stop_event.wait(60)
+        idle_event.set()
 
 def setup(config:dict):
     main_logger.log_info(f'Setting up NVR...')
@@ -118,20 +129,27 @@ def setup(config:dict):
     main_logger.log_info(f'Setting up limit checkers...')
     limit_checkers = setup_limit_checkers(recorders, config)
 
-    is_running = threading.Event()
+    # Event for when the limits thread should stop
+    # Needs to be a set-to-stop for the sleep to work
+    stop_event = threading.Event()
     are_checkers_idle = threading.Event()
 
-    limit_threads = create_threads([ lambda: start_limit_checkers(limit_checkers, is_running, are_checkers_idle) ])
+    # Stopped by default
+    stop_event.set()
+
+    limit_threads = create_threads([ lambda: start_limit_checkers(limit_checkers, stop_event, are_checkers_idle) ])
     recorder_start_threads = create_threads([ recorder.start for recorder in recorders.values() ])
     recorder_stop_threads = create_threads([ recorder.stop for recorder in recorders.values() ])
 
     def start():
         main_logger.log_info('Starting NVR...')
 
-        if is_running.is_set():
+        # If not stopped, aka running
+        if not stop_event.is_set():
             raise Exception(f'NVR is already running!')
         
-        is_running.set()
+        # Clear stop, aka start
+        stop_event.clear()
 
         main_logger.log_info('Starting recorders...')
         start_threads(recorder_start_threads)
@@ -145,7 +163,7 @@ def setup(config:dict):
 
     def stop():
         main_logger.log_info('Stopping NVR...')
-        is_running.clear()
+        stop_event.set()
 
         main_logger.log_info('Stopping recorders...')
         start_threads(recorder_stop_threads)
@@ -160,8 +178,9 @@ def setup(config:dict):
 
 if __name__ == '__main__':
     try:
+        main_logger.log_info(f'Reading config from {DEFAULT_CONFIG_FILE}...')
+        
         try:
-            main_logger.log_info(f'Reading config from {DEFAULT_CONFIG_FILE}...')
             config = yaml.safe_load(open(DEFAULT_CONFIG_FILE, 'r'))
         except FileNotFoundError as e:
             raise Exception(f'Config file at "{DEFAULT_CONFIG_FILE}" does not exist!')
